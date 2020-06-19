2890b4406d25f864dc2e40040f86fa523cb1fd6b
Ruma Das <rumaadl16@gmail.com>
2020-06-20 03:31:17 +0930

    adding schema table

---
+++ models/workout.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const WorkoutSchema = new Schema({
+exercises: [{
+    name: {
+        type: String,
+        trim: true,
+        required: "Exercise name is Required"
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number,
+            required: "Enter duration in Minutes"
+        },
+        distance: {
+            type: Number,
+            required: "Enter distance in miles"
+        }
+
+}
+ 
+]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
