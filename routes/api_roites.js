6960d15e31d827c5a8976b2ca6c3580c52a9cbd8
Ruma Das <rumaadl16@gmail.com>
2020-06-23 01:02:25 +0930

    Adding post api

--- routes/api_roites.js
+++ routes/api_roites.js
@@ -1,11 +1,19 @@
 var db = require("../models");
 
 module.exports = function(app) {
-   app.get("/api/wokouts", function(req,res){
+   app.get("/api/workouts", function(req,res){
        db.Workout.find({}).then(dbWorkout =>{
            res.json(dbWorkout);
        }).catch(err =>{
            res.json(err);
        });
    });
+
+   app.post("/api/workouts", function(req,res){
+    db.Workout.create({}).then(dbWorkout =>{
+        res.json(dbWorkout);
+    }).catch(err =>{
+        res.json(err);
+    });
+});
 }
