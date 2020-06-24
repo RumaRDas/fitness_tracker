25535788a4f5e93dbec21d76bd4309339c20b857
Ruma Das <rumaadl16@gmail.com>
2020-06-25 00:03:39 +0930

    Put and get api Adding

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -2,10 +2,11 @@ var db = require("../models");
 
 module.exports = function (app) {
   app.get("/api/workouts", function (req, res) {
-    db.Workout.find({}).then(dbWorkout => {
+    db.Workout.find({}).sort({ day: -1 })
+    .then(dbWorkout => {
       res.json(dbWorkout);
     }).catch(err => {
-      res.json(err);
+      res.status(400).json(err);
     });
   });
 
@@ -15,8 +16,42 @@ module.exports = function (app) {
         res.json(dbWorkout);
       })
       .catch(err => {
-        res.json(err);
+        res.status(400).json(err);
       });
   });
 
+  app.put("/api/workouts/:id", (req, res, next) => {
+    const id = req.params.id;
+    const data = req.body
+    db.Workout.updateOne({ _id: id }, {
+      $push: {
+        exercises: [
+          {
+            "type": data.type,
+            "name": data.name,
+            "duration": data.duration,
+            "distance": data.distance,
+            "weight": data.weight,
+            "reps": data.reps,
+            "sets": data.sets
+          }
+        ]}
+    })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  app.get("/api/workouts/range", function(req, res) {
+    db.Workout.find({}).limit(5)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 }
